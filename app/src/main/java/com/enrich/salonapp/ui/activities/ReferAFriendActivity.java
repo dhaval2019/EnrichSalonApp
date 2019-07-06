d9c29dc7334840dafc850abab677ac7108b63398
dhaval2019 <dhaval.shah@enrichsalon.com>
2019-07-06 13:59:17 +0530

    refer a friend icon placed

---
+++ app/src/main/java/com/enrich/salonapp/ui/activities/ReferAFriendActivity.java
@@ -0,0 +1,16 @@
+package com.enrich.salonapp.ui.activities;
+
+
+import android.os.Bundle;
+
+import com.enrich.salonapp.R;
+import com.enrich.salonapp.util.mvp.BaseActivity;
+
+public class ReferAFriendActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refer_afriend);
+    }
+}
