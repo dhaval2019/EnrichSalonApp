4c283dce161eeb353787f6038d7df4de8c6b4fb0
dhaval2019 <dhaval.shah@enrichsalon.com>
2019-07-07 17:30:03 +0530

    select friend done

--- app/src/main/java/com/enrich/salonapp/ui/activities/ReferAFriendActivity.java
+++ app/src/main/java/com/enrich/salonapp/ui/activities/ReferAFriendActivity.java
@@ -1,16 +1,34 @@
 package com.enrich.salonapp.ui.activities;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.enrich.salonapp.R;
 import com.enrich.salonapp.util.mvp.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ReferAFriendActivity extends BaseActivity {
+    @BindView(R.id.invite_button)
+    Button btnInvite;//by dhaval shah 7/7/19
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refer_afriend);
+        ButterKnife.bind(this);
+        btnInvite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReferAFriendActivity.this, SelectFriendActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
