2b466fb883575547db8c1a2355686aa2c1f7d580
dhaval2019 <dhaval.shah@enrichsalon.com>
2019-07-22 12:34:21 +0530

    select friend done

--- app/src/main/java/com/enrich/salonapp/ui/activities/ReferAFriendActivity.java
+++ app/src/main/java/com/enrich/salonapp/ui/activities/ReferAFriendActivity.java
@@ -4,8 +4,12 @@ package com.enrich.salonapp.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.enrich.salonapp.R;
 import com.enrich.salonapp.util.mvp.BaseActivity;
@@ -16,17 +20,39 @@ import butterknife.ButterKnife;
 public class ReferAFriendActivity extends BaseActivity {
     @BindView(R.id.invite_button)
     Button btnInvite;//by dhaval shah 7/7/19
-
+    @BindView(R.id.tvfrndlink)
+    TextView txtFriendLink;
+    @BindView(R.id.popuplayout)
+    RelativeLayout popupLayout;
+    @BindView(R.id.tvclose)
+    TextView txtClose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refer_afriend);
         ButterKnife.bind(this);
+        popupLayout.setVisibility(View.GONE);
         btnInvite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ReferAFriendActivity.this, SelectFriendActivity.class);
                 startActivity(intent);
+            }
+        });
+        txtFriendLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupLayout.setVisibility(View.VISIBLE);
+                Animation animation;
+                animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.bottom_to_original);
+                popupLayout.setAnimation(animation);
+            }
+        });
+        txtClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupLayout.setVisibility(View.GONE);
             }
         });
 
