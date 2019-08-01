1e64cb1e93f5f364e22c4bf96f40f2a18356b187
dhaval2019 <dhaval.shah@enrichsalon.com>
2019-08-01 14:11:22 +0530

    fb event done for friend referal

---
+++ app/src/main/java/com/enrich/salonapp/data/model/Referal.java
@@ -0,0 +1,27 @@
+package com.enrich.salonapp.data.model;
+
+public class Referal {
+    public Referal()
+    {
+
+    }
+    String referalName;
+
+    public String getReferalName() {
+        return referalName;
+    }
+
+    public void setReferalName(String referalName) {
+        this.referalName = referalName;
+    }
+
+    public String getReferalContactNo() {
+        return referalContactNo;
+    }
+
+    public void setReferalContactNo(String referalContactNo) {
+        this.referalContactNo = referalContactNo;
+    }
+
+    String referalContactNo;
+}
