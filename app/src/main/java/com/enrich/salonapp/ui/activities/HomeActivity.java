4d2d240b7f4397cf2a1f1672519f725c7f304873
dhaval2019 <dhaval.shah@enrichsalon.com>
2019-09-24 18:23:43 +0530

    rdeem your code screen done

--- app/src/main/java/com/enrich/salonapp/ui/activities/HomeActivity.java
+++ app/src/main/java/com/enrich/salonapp/ui/activities/HomeActivity.java
@@ -118,7 +118,8 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
 
     @BindView(R.id.refer_a_friend_container)
     RelativeLayout referAFriendContainer;//by dhaval shah 6/7/19
-
+    @BindView(R.id.redeem_your_code_container)
+    RelativeLayout redeemYourCodeContainer;//by dhaval shah 24/9/19
     CenterDetailModel centerDetailModel;
 
     GuestModel guestModel;
@@ -283,6 +284,13 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, ReferAFriendActivity.class);
+                startActivity(intent);
+            }
+        });
+        redeemYourCodeContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, CodeRedeemActivity.class);
                 startActivity(intent);
             }
         });
