edbf9ac011f39a74995d16f8ab5954013b1fabb9
dhaval2019 <dhaval.shah@enrichsalon.com>
2019-09-25 16:18:50 +0530

    coderedeemregistration included

---
+++ app/src/main/java/com/enrich/salonapp/ui/activities/RegistrationCodeRedeemActivity.java
@@ -0,0 +1,36 @@
+
+package com.enrich.salonapp.ui.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.enrich.salonapp.R;
+import com.enrich.salonapp.util.EnrichUtils;
+
+public class RegistrationCodeRedeemActivity extends AppCompatActivity {
+    boolean isFromLoginLater;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_code_redeem);
+        isFromLoginLater = getIntent().getBooleanExtra("IsFromLoginLater", false);
+        if (!isFromLoginLater) {
+            if (EnrichUtils.getHomeStore(this) != null) {
+                Intent intent = new Intent(RegistrationCodeRedeemActivity.this, HomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(intent);
+                finish();
+            } else {
+                Intent intent = new Intent(RegistrationCodeRedeemActivity.this, StoreSelectorActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(intent);
+                finish();
+            }
+        } else {
+            finish();
+        }
+    }
+}
