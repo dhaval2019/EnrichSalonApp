a1c055e6526291fd9e98f3186ebd8d78d38c8c45
dhaval2019 <dhaval.shah@enrichsalon.com>
2019-10-11 15:32:46 +0530

    google analytics done

--- app/src/main/java/com/enrich/salonapp/ui/activities/BigDaysActivity.java
+++ app/src/main/java/com/enrich/salonapp/ui/activities/BigDaysActivity.java
@@ -3,13 +3,22 @@ package com.enrich.salonapp.ui.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.enrich.salonapp.EnrichApplication;
 import com.enrich.salonapp.R;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 
 public class BigDaysActivity extends AppCompatActivity {
-
+    Tracker mTracker;
+    EnrichApplication application;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_big_days);
+        application = (EnrichApplication) getApplication();
+        mTracker = application.getDefaultTracker();
+        mTracker.setScreenName("Beauty and Bling winning screen");
+        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+        mTracker.enableAdvertisingIdCollection(true);
     }
 }
